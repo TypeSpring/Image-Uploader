a0a7a53ec3f4c9fb089c84f80f24291c33c0a658
Hyevvy <72402747+Hyevvy@users.noreply.github.com>
2022-03-21 02:12:46 +0900

    feat: bulk 이미지 업로드 구현 (#2)

--- src/components/MyProfileImg.tsx
+++ src/components/MyProfileImg.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import ImageUploading, { ImageListType } from "react-images-uploading";
 import { request } from "../config/axios";
 
 export function MyProfileImg() {
   const [images, setImages] = React.useState([]);
   const [url, setUrl] = React.useState("");
+  const [bulkUrl, setBulkUrl] = React.useState([]);
   const maxNumber = 69;
 
   const onChange = (
@@ -16,6 +17,7 @@ export function MyProfileImg() {
     setImages(imageList as never[]);
   };
 
+  //단건 이미지 업로드
   const onImageServerUpload = async (imageList: ImageListType) => {
     console.log(imageList);
     console.log("서버에 보냅니다.");
@@ -31,8 +33,34 @@ export function MyProfileImg() {
         },
       })
       .then((res) => {
+        setBulkUrl(res.data);
+      });
+  };
+
+  // bulk 이미지 업로드
+  const onImageBulkServerUpload = async (imageList: ImageListType) => {
+    console.log(imageList);
+    console.log("서버에 보냅니다.");
+
+    const formData = new FormData();
+    if (imageList == undefined || imageList[0].file == undefined) {
+      return;
+    }
+    imageList.map((it) => {
+      if (it.file == undefined) {
+        return;
+      }
+      formData.append("file", it.file);
+    });
+    await request
+      .post("/images/bulk", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
         console.log(res.data);
-        setUrl(res.data);
+        setBulkUrl(bulkUrl.concat(res.data));
       });
   };
 
@@ -53,41 +81,64 @@ export function MyProfileImg() {
           isDragging,
           dragProps,
         }) => (
-          // write your building UI
           <div className="upload__image-wrapper">
-            <button
-              style={isDragging ? { color: "red" } : undefined}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button>
-            &nbsp;
-            <button onClick={onImageRemoveAll}>Remove all images</button>
+            <div className="upload__image-wrapper__btns">
+              <button
+                style={isDragging ? { color: "red" } : undefined}
+                onClick={onImageUpload}
+                {...dragProps}
+              >
+                Click or Drop here
+              </button>
+              &nbsp;
+              <button onClick={onImageRemoveAll}>Remove all images</button>
+            </div>
             {imageList.map((image, index) => (
               <div key={index} className="image-item">
                 <img src={image.dataURL} alt="" width="100" />
                 <div className="image-item__btn-wrapper">
                   <button onClick={() => onImageUpdate(index)}>Update</button>
                   <button onClick={() => onImageRemove(index)}>Remove</button>
+                  {imageList[0] && (
+                    <button onClick={() => onImageServerUpload(imageList)}>
+                      단건 Server Upload
+                    </button>
+                  )}
                 </div>
               </div>
             ))}
-            <button onClick={() => onImageServerUpload(imageList)}>
-              Server Upload
-            </button>
+            {imageList[0] && (
+              <button onClick={() => onImageBulkServerUpload(imageList)}>
+                전체 Server Upload
+              </button>
+            )}
           </div>
         )}
       </ImageUploading>
 
       {url && (
         <div>
-          서버에서 받은 이미지
+          서버에서 받은 이미지 (단건)
           <img
             alt="서버에서 받은 이미지입니다"
             src={url}
             style={{ width: "100px", height: "100px" }}
           />
+        </div>
+      )}
+
+      {bulkUrl[0] && (
+        <div>
+          서버에서 받은 이미지 (Bulk)
+          {console.log(bulkUrl)}
+          {bulkUrl.map((it) => (
+            <img
+              alt="서버에서 받은 이미지입니다"
+              key={it}
+              src={it}
+              style={{ width: "100px", height: "100px" }}
+            />
+          ))}
         </div>
       )}
     </div>
